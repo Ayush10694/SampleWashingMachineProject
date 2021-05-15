971efe6dfc8b40273eeca705c3800a8c8cf30cc3
Ayush10694 <84224297+Ayush10694@users.noreply.github.com>
2021-05-15 17:07:06 +0530

    Update SampleMachineTest.java

--- src/test/java/com/test/SampleMachineTest.java
+++ src/test/java/com/test/SampleMachineTest.java
@@ -1,16 +1,12 @@
 package com.test;
 
-import static org.junit.Assert.assertThat;
-
-import java.awt.PageAttributes.MediaType;
+import static org.junit.Assert.assertEquals;
 import java.util.Optional;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -50,7 +46,7 @@ public class SampleMachineTest {
 		MvcResult result = mockMvc.perform(req).andReturn();
 		String expOut= this.mapToJson(obj);
 		String out = result.getResponse().getContentAsString();
-		//assertThat(out);
+		assertEquals(expOut, out);
 	
 	}
 	private String mapToJson(Object obj) throws JsonProcessingException
